ce8f876703917539d73f937160b52d79ec2e1d80
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2023-02-12 15:41:03 +0100

    first commit

---
+++ src/main/java/org/yascode/rabbitmqperso/RabbitmqPersoApplication.java
@@ -0,0 +1,13 @@
+package org.yascode.rabbitmqperso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPersoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqPersoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yascode/rabbitmqperso/RabbitmqPersoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yascode.rabbitmqperso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqPersoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
